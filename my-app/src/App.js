9efadbf85e1f638affc6ea06ef0578117719e605
jeff-yc-wong <42687471+jeff-yc-wong@users.noreply.github.com>
2020-07-03 01:44:03 -1000

    Included user input to .ics file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,11 +4,16 @@ import 'semantic-ui-css/semantic.min.css'
 import { Grid, Header, Form, TextArea, Menu } from 'semantic-ui-react';
 
 class App extends React.Component {
-    state = {eventName: "", eventLocation: "", start: "", end: "", }
+    state = {eventName: "", eventLocation: "", start: "", end: "", eventDescription: "" };
 
-    submit = (e) => {
+    submit = (e, {formData}) => {
+        e.preventDefault();
         let element = document.createElement('a');
-
+        let date = new Date();
+        let currentDateTime = date.getFullYear() + '' +
+            + '' + (date.getMonth() + 1) + '' + ("0" + date.getDate()).slice(-2) + 'T' +
+            ("0" + date.getHours()).slice(-2) + '' + ("0" + date.getMinutes()).slice(-2) + '' +
+            ("0" + date.getSeconds()).slice(-2);
         element.setAttribute('href', 'data:text/calendar;charset=utf-8,' +
             //Starting Calendar
             'BEGIN:VCALENDAR\n' +
@@ -22,25 +27,23 @@ class App extends React.Component {
             'BEGIN:VEVENT\n' +
             'SEQUENCE:0\n' +
             // Time event was created
-            'DTSTAMP:20200627T123349Z\n' +
+            'DTSTAMP:' + currentDateTime + '\n' +
             //Start time of event
-            'DTSTART:20200704\n' +
+            'DTSTART:' + this.state.start.split('-').join('').replace(':', '') + '00\n' +
             //End time of event
-            'DTEND:202000704\n' +
+            'DTEND:' + this.state.end.split('-').join('').replace(':', '') + '00\n' +
             //Event Name
-            'SUMMARY:Test123\n' +
+            'SUMMARY:' + this.state.eventName + '\n' +
             //Event Description
-            'DESCRIPTION:foo\n' +
+            'DESCRIPTION:' + this.state.eventDescription + '\n' +
             //Event Location
-            'LOCATION:hawaii\n' +
+            'LOCATION:' + this.state.eventLocation + '\n' +
             //Ending Event
             'END:VEVENT\n' +
             //Ending Calendar
             'END:VCALENDAR');
 
-        element.setAttribute('download', "test.ics");
-
-
+        element.setAttribute('download', this.state.eventName + ".ics");
 
         element.style.display = 'none';
         document.body.appendChild(element);
@@ -49,8 +52,13 @@ class App extends React.Component {
 
         document.body.removeChild(element);
 
-
+        this.setState({eventName: "", eventLocation: "", start: "", end: "", eventDescription: "" });
     };
+
+    handleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value});
+        console.log(e.target.value.split('-').join('').replace(':', ''));
+    }
 
     render() {
         return (
@@ -62,13 +70,13 @@ class App extends React.Component {
         <Grid.Column>
         <Header as="h2" textAlign="center" className='white'>Add an Event!</Header>
         <Form onSubmit={this.submit}>
-            <Form.Input fluid  label='Event Name' placeholder='Ex. New Year Party' />
+            <Form.Input fluid  name='eventName' value={this.state.eventName} label='Event Name' placeholder='Ex. New Year Party' onChange={this.handleChange}/>
             <Form.Group widths='equal'>
-            <Form.Input   label='Location' placeholder='Ex. 1234 Foo St. Honolulu, HI 96821' />
-            <Form.Input  label=' Start Date' type='datetime-local' />
-            <Form.Input  label=' End Date' type='datetime-local' />
+            <Form.Input name='eventLocation' label='Location' placeholder='Ex. 1234 Foo St. Honolulu, HI 96821' onChange={this.handleChange}/>
+            <Form.Input  name='start' label=' Start Date' type='datetime-local' onChange={this.handleChange}/>
+            <Form.Input  name='end' label=' End Date' type='datetime-local' onChange={this.handleChange}/>
             </Form.Group>
-            <Form.Input control={TextArea}  label='Event Description' placeholder='Ex. New Year Party' />
+            <Form.Input name='eventDescription' control={TextArea}  label='Event Description' placeholder='Ex. New Year Party' onChange={this.handleChange}/>
             <Form.Button>Submit</Form.Button>
         </Form>
         <br/>
